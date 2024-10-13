9818fa0c8e481560647b2178ee966199b790bbdb
zhouzw47951 <zhouzw47951@hundsun.com>
2024-10-13 16:12:04 +0800

    1

--- front-server-admin/src/views/workers/components/editBox.vue
+++ front-server-admin/src/views/workers/components/editBox.vue
@@ -3,11 +3,11 @@
     <el-dialog
       title="扫码录入"
       :visible.sync="showDialog"
-      width="500"
+      width="600px"
       @close="handleClose"
     >
       <div style="text-align: center">
-        <canvas id="QRCode_header" />
+        <img id="img" style="width: 400px" :src="url" alt="">
       </div>
       <!--      关闭按钮-->
       <div slot="footer" class="footer">
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import QRCode from 'qrcode'
+// import QRCode from 'qrcode'
 export default {
   name: 'EditBox',
   props: {
@@ -36,7 +36,9 @@ export default {
   data() {
     return {
       saveLoading: false,
-      QRCodeMsg: ''
+      QRCodeMsg: '',
+      userInfo: {},
+      url: ''
     }
   },
   computed: {
@@ -54,30 +56,23 @@ export default {
       val && this.initData()
     }
   },
+  created() {
+    this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+  },
   methods: {
     handleClose() {
       this.showDialog = false
     },
     async initData() {
       await this.$nextTick()
-      const opts = {
-        errorCorrectionLevel: 'H', // 容错级别
-        type: 'image/png', // 生成的二维码类型
-        quality: 0.3, // 二维码质量
-        margin: 0, // 二维码留白边距
-        width: 200, // 宽
-        height: 180, // 高
-        text: 'http://www.xxx.com', // 二维码内容
-        color: {
-          dark: '#333', // 前景色
-          light: '#eee'// 背景色
+      const url = `https://api.bmxt.info/api/auth/servicePoint/${this.userInfo.service_point_id}`
+      this.$request.get(url).then(res => {
+        if (res.success) {
+          // 将URL赋值给<img>标签的src属性
+          document.getElementById('img').src = res.data
+        } else {
+          this.$message.error(res.message)
         }
-      }
-      this.QRCodeMsg = 'http://www.baidu.com' // 生成的二维码为URL地址js
-      const msg = document.getElementById('QRCode_header')
-      // 将获取到的数据（val）画到msg（canvas）上
-      QRCode.toCanvas(msg, this.QRCodeMsg, opts, function(error) {
-        console.log(error)
       })
     }
   }
